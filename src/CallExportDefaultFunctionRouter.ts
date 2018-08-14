a86c4f3884ecc5bf43dc4466f004cd4c2da77dee
YAN Yuchen <cirahnitex@gmail.com>
2018-08-14 18:27:34 +0800

    remove hot reload

--- src/CallExportDefaultFunctionRouter.ts
+++ src/CallExportDefaultFunctionRouter.ts
@@ -1,12 +1,11 @@
 import {Express} from "express";
 import {join} from "path";
-const reload:(path:string)=>any = require('require-reload')(require);
 
 export function callExportDefaultFunctionRouter(app:Express, apiRoot:string) {
     app.post(/^.*$/,async (req,res)=>{
         res.contentType("application/json");
         try {
-            const api = reload(join(apiRoot, req.path)).default;
+            const api = require(join(apiRoot, req.path)).default;
             const value = await api(req.body);
             res.send(JSON.stringify({
                 success: true,
