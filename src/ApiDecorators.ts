eb0a30ee44441b3036635f9167f38f9984c4adef
YAN Yuchen <cirahnitex@gmail.com>
2018-09-14 14:43:21 +0800

    fix isAdmin check

--- src/ApiDecorators.ts
+++ src/ApiDecorators.ts
@@ -1,5 +1,5 @@
 
-export function requireAdmin(api:(param:Record<string,string>)=>Promise<any>) {
+export function requireAdmin(api:(param:{[key:string]:string})=>Promise<any>) {
     return (param:any)=>{
         if(param.REMOTE_USER !== '1') throw new Error("PERMISSION_DENIED");
         return api(param);
